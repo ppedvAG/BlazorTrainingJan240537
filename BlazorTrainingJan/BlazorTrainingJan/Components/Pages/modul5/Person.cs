928d45be7b18ade7e952864e0086358a1d0ba5d1
Hannes Preishuber <hannesp@ppedv.de>
2024-01-24 14:47:05 +0100

    Edit Formular

---
+++ BlazorTrainingJan/BlazorTrainingJan/Components/Pages/modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingJan.Components.Pages.modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="bitte füllen")]
+        [StringLength(10,ErrorMessage ="maximal 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "bitte füllen")]     
+        [Range(150,190,ErrorMessage ="zwischen {0} und {1}")]
+        public int Gross { get; set; }
+
+        [Required(ErrorMessage = "bitte füllen")]
+        public DateTime GebDate { get; set; }
+    }
+}
