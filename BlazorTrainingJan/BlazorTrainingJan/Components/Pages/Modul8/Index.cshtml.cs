4848e7e26fe0b73997d90a934def4b7bf1f8abcb
Hannes Preishuber <hannesp@ppedv.de>
2024-01-25 13:50:09 +0100

    wie JavaScript Funktioniert

---
+++ BlazorTrainingJan/BlazorTrainingJan/Components/Pages/Modul8/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingJan.Components.Pages.Modul8
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
